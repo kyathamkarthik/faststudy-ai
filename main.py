957ae6114579d29ff83150eee841dca33da2fd66
kyathamkarthik <kyathamkarthik@64@gmail.com>
2026-04-11 12:15:49 +0530

    first version of FastStudy AI

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from openai import OpenAI
+
+app = FastAPI()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+class Query(BaseModel):
+    question: str
+    mode: str
+
+def build_prompt(q, mode):
+    if mode == "short":
+        return f"Answer in 2-3 lines only: {q}"
+    elif mode == "exam":
+        return f"Give bullet points for exam: {q}"
+    else:
+        return f"Explain simply like a friend: {q}"
+
+@app.get("/")
+def home():
+    return {"message": "FastStudy AI is running 🚀"}
+
+@app.post("/ask")
+def ask_ai(query: Query):
+    prompt = build_prompt(query.question, query.mode)
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=120
+    )
+
+    return {"answer": response.choices[0].message.content}
